7ae6ce9df6e7d13689a61bc9a9b38f493883915f
Dhananjay17728 <Dhananjay17728@gmail.com>
2021-08-21 12:38:45 +0530

    task 2 website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./navbar";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
+};
+
+export default App;
